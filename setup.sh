f6f3083bb05799c7818b6de6cb59364f5c179c51
ideyuta <ide.yuta@gmail.com>
2017-06-16 09:00:21 +0900

    fix(setup): Fix setup script

--- setup.sh
+++ setup.sh
@@ -15,14 +15,14 @@ ln -si $BASE_DIR/tmux.conf $HOME/.tmux.conf
 ln -si $BASE_DIR/vimrc $HOME/.vimrc
 ln -si $BASE_DIR/vim $HOME/.vim
 ln -si $BASE_DIR/gvimrc $HOME/.gvimrc
-ln -si $BASE_DIR/deoplete.rc.vim $HOME/.vim/deoplete.rc.vim
-ln -si $BASE_DIR/neomake.rc.vim $HOME/.vim/neomake.rc.vim
+ln -si $BASE_DIR/deoplete.rc.vim $HOME/deoplete.rc.vim
+ln -si $BASE_DIR/neomake.rc.vim $HOME/neomake.rc.vim
 
 # Zsh
-ln -s $BASE_DIR/zshrc $HOME/.zshrc
+ln -si $BASE_DIR/zshrc $HOME/.zshrc
 
 # ESLint
-ln -s $BASE_DIR/eslintrc $HOME/.eslintrc
+ln -si $BASE_DIR/eslintrc $HOME/.eslintrc
 
 
 ln -si $BASE_DIR/dein.toml $HOME/.dein.toml
